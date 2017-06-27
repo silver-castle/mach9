9ffb3bfcdafc0a55d047f9bfa86926f1169a4a57
38elements <38elements@users.noreply.github.com>
2017-06-27 21:24:04 +0900

    Revert "0.0.3 (#11)" (#12)
    This reverts commit 085d86aec8eaf267a5fa68a865b6b151e9dcc841.

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ from setuptools import setup
 setup_kwargs = {
     'name': 'mach9',
     'author': '38elements',
-    'version': '0.0.3',
-    'url': 'https://github.com/silver-castle/mach9',
+    'version': '0.0.2',
     'license': 'MIT License',
     'packages': ['mach9'],
     'classifiers': [
