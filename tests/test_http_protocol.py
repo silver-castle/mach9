1b4871387b087df3debfe834a150db9b9145a777
38elements <38elements@users.noreply.github.com>
2017-06-29 22:56:35 +0900

    Add protocol.is_response_chunk() (#18)

--- tests/test_http_protocol.py
+++ tests/test_http_protocol.py
@@ -28,3 +28,16 @@ def test_check_headers():
     ])
     assert result_headers['connection_close'] is False
     assert result_headers['content_length'] is True
+
+
+def test_is_reponse_chunk():
+    protocol = HttpProtocol(loop=None, request_handler=None,
+                            error_handler=None)
+    result = protocol.is_response_chunk({'a': 1})
+    assert result is True
+    result = protocol.is_response_chunk({'status': 1})
+    assert result is False
+    result = protocol.is_response_chunk({'headers': 1})
+    assert result is False
+    result = protocol.is_response_chunk({'status': 1, 'headers': 1})
+    assert result is False
